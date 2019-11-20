f4a91329e1284b1ad58f80c390f20edededb6718
bext <jalbertomr@yahoo.com>
2019-11-19 21:00:47 -0600

    @DisplayNameGeneration @Nested corrected

--- src/test/java/DisplayNameGeneratorDemo.java
+++ src/test/java/DisplayNameGeneratorDemo.java
@@ -9,23 +9,24 @@ public class DisplayNameGeneratorDemo {
     @DisplayNameGeneration(IndicativeSentences.ReplaceUnderscores.class)
     class A_year_is_not_supported {
         @Test
-        void if_it_is_zero(){  }
+        void if_it_is_zero(){ /* validation code here*/ }
 
         @DisplayName("Un valor negativo para el año no es soportado por el computo de año bisiesto.")
         @ParameterizedTest(name = "Ejemplo, año {0} no es soportado.")
         @ValueSource( ints = { -1, -4})
-        void if_it_is_negative(int year){  }
+        void if_it_is_negative(int year){  /* validation code here*/ }
     }
 
     @Nested
-    @DisplayNameGeneration(IndicativeSentences.class)
+    @DisplayNameGeneration(IndicativeSentences.ReplaceUnderscores.class)
     class A_year_is_a_leap_year {
         @Test
-        void if_it_is_divisible_by_4_but_not_by_100(){  }
+        void if_it_is_divisible_by_4_but_not_by_100(){ /* validation code here*/ }
 
+        @DisplayName("....")
         @ParameterizedTest(name = "Año {0} es año bisiesto.")
         @ValueSource( ints = { 2016, 2020, 2048})
-        void  if_it_is_one_of_the_following_years(int year){ }
+        void  if_it_is_one_of_the_following_years(int year){ /* validation code here*/ }
     }
 
 
